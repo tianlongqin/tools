1bf1d2347772b4a6a152d71a382c63c128f4edfa
tianlongqin <qtl_linux@163.com>
2020-04-21 11:25:18 +0800

    file: support get file and directory count fucntion
    Signed-off-by: tianlongqin <qtl_linux@163.com>

--- file/file.c
+++ file/file.c
@@ -26,51 +26,20 @@
  * Created on: 2019-08-19
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+#include <libgen.h>
 
 
+#include "file.h"
 /*
- * Tfile_check_type_fd - check file type 
- * @fd: file descriptor 
- *
- * On success, type is returned.  On error, -1 is returned, and errno is set appropriately.
- */
-int Tfile_check_type_fd(int fd, int type)
-{
-	struct stat stat;
-
-	if (fstat(fd, &stat) < 0) {
-		perror("fstat error:");
-		return -1;
-	}
-
-	return stat.st_mode & type;
-}
-
-/*
- * Tfile_check_type_name - check file type 
- * @fd: file descriptor 
- *
- * On success, type is returned.  On error, -1 is returned, and errno is set appropriately.
- */
-int Tfile_check_type_name(const char *name, int type)
-{
-	struct stat st;
-
-	if (stat(name, &st) < 0) {
-		perror("fstat error:");
-		return -1;
-	}
-
-	return st.st_mode & type;
-}
-
-/*
- * Tfile_get_size_fd - get file tatol size 
+ * Tfile_get_size_fd - get file tatol size
  * @fd: file descriptor 
  *
  * On success, size is returned.  On error, -1 is returned, and errno is set appropriately.
@@ -83,12 +52,12 @@ int Tfile_get_size_fd(int fd)
 		perror("fstat error:");
 		return -1;
 	}
-	
+
 	return stat.st_size;
 }
 
 /*
- * Tfile_get_size_name - get file tatol size 
+ * Tfile_get_size_name - get file tatol size
  * @name: file name
  *
  * On success, size is returned.  On error, -1 is returned, and errno is set appropriately.
@@ -102,4 +71,74 @@ int Tfile_get_size_name(const char *name)
 	}
 
 	return st.st_size;
+}
+
+/*
+ * Tfile_get_file_count - get file and directory count
+ * @path: file path
+ * @dcount: directory count
+ * @fcount: file count
+ * @flags: reserved
+ *
+ * On success, 0 is returned.  On error, -1 is returned, and errno is set appropriately.
+ */
+int Tfile_get_file_count(const char *path, uint32_t *dcount, uint32_t *fcount, int flags)
+{
+	int ret;
+	uint32_t _dcount = 0, _fcount = 0;
+	DIR *dir;
+	struct dirent *ptr;
+	struct stat st;
+	char dname[PATH_MAX];
+	char linkname[PATH_MAX];
+	char linkname1[PATH_MAX];
+
+	dir = opendir(path);
+	if (!dir) {
+		perror("opendir:");
+		return -1;
+	}
+
+	while ((ptr = readdir(dir)) != NULL) {
+		if (strcmp(ptr->d_name, ".") == 0 || strcmp(ptr->d_name, "..") == 0)
+			continue;
+
+		/* contain hidden file and hidden dir*/
+		if(ptr->d_name[0] == '.')
+			continue;
+
+		snprintf(dname, sizeof(dname) - 1, "%s/%s", path, ptr->d_name);
+
+		ret = lstat(dname, &st);
+		if (ret < 0)
+			return -1;
+
+		if (S_ISLNK(st.st_mode)) {
+			memset(linkname, 0, PATH_MAX);
+			readlink(dname, linkname, PATH_MAX);
+			ret = lstat(linkname, &st);
+			if (ret < 0) {
+				snprintf(linkname1, sizeof(linkname) - 1, "%s/%s", dirname(dname), linkname);
+				ret = lstat(linkname1, &st);
+			}
+
+			if (S_ISDIR(st.st_mode))
+				*dcount += 1;
+			else
+				*fcount += 1;
+
+			continue;
+		}
+
+		if (S_ISDIR(st.st_mode)) {
+			Tfile_get_file_count(dname, dcount, fcount, flags);
+			*dcount += 1;
+		} else {
+			*fcount += 1;
+		}
+	}
+
+	closedir(dir);
+
+	return 0;
 }
--- file/file.h
+++ file/file.h
@@ -1,22 +1,13 @@
 #ifndef __FILE_HH_10245__
 #define __FILE_HH_10245__
 
-#define F_MT		S_IFMT //bit mask for the file type bit field
-#define F_SOCK		S_IFSOCK // socket
-#define F_LINK		S_IFLNK //symbolic link
-#define F_REG		S_IFREG //regular file
-#define F_BLK		S_IFBLK // block device
-#define F_DIR		S_IFDIR // directory
-#define F_CHR		S_IFCHR //character device
-#define F_FIFO		S_IFIFO // FIFO
 #ifdef __cplusplus 
 extern "C" {
 #endif
 
-extern int Tfile_check_type_fd(int fd, int type);
-extern int Tfile_check_type_name(const char *name, int type);
 extern int Tfile_get_size_fd(int fd);
 extern int Tfile_get_size_name(const char *name);
+extern int Tfile_get_file_count(const char *path, uint32_t *dcount, uint32_t *fcount, int flags);
 
 #ifdef __cplusplus 
 }
